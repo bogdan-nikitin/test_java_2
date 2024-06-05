bd57998fe36ebb84df895d79b1a61d7b9b4e6f27
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-06-05 22:38:39 +0300

    Tests

---
+++ src/test/java/scene/SceneTest.java
@@ -0,0 +1,68 @@
+package scene;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SceneTest {
+    private static final List<String> PERSON = List.of(
+            "Chandler", "Joey", "Monica", "Phoebe", "Rachel", "Ross"
+    );
+    Random random = new Random();
+
+    void test(final List<String> input) {
+        final String joined = String.join(System.lineSeparator(), input);
+        final byte[] in = joined.getBytes(StandardCharsets.UTF_8);
+        final Scene scene = new Scene();
+        try {
+            scene.read(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(in))));
+            final ByteArrayOutputStream out = new ByteArrayOutputStream(in.length);
+            final OutputStreamWriter writer = new OutputStreamWriter(out);
+            scene.write(writer);
+            writer.flush();
+            assertEquals(joined, out.toString(StandardCharsets.UTF_8).strip());
+        } catch (final IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    String randomText() {
+        return String.valueOf(random.nextInt());
+    }
+
+    Stream<String> generateInput(final int size) {
+        return Stream.generate(() -> STR."\{PERSON.get(random.nextInt(PERSON.size()))}: \{randomText()}").limit(size);
+    }
+
+    void testInvalid(final String invalidLine) {
+        assertThrows(IllegalArgumentException.class, () -> test(Stream.of(generateInput(5),
+                Stream.of(invalidLine),
+                generateInput(5)).reduce(Stream::concat).get().toList()));
+    }
+
+    @Test
+    @DisplayName("Script where one line doesn't have person")
+    void test_noPerson() {
+        testInvalid(randomText());
+    }
+
+    @Test
+    @DisplayName("Script where one line has unknown person")
+    void test_wrongPerson() {
+        testInvalid(STR."Elon: \{randomText()}");
+    }
+
+    @Test
+    @DisplayName("Check valid script")
+    void test_valid() {
+        IntStream.range(0, 1000).forEach(ignored -> test(generateInput(1000).toList()));
+    }
+}
