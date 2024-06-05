80e99464edbdede18ac21986a638620444e0ef10
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-06-05 15:23:03 +0300

    Refactor

--- src/main/java/scene/Scene.java
+++ src/main/java/scene/Scene.java
@@ -1,25 +1,34 @@
 package scene;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Scene {
     private static final Set<String> PERSON = Set.of(
             "Chandler", "Joey", "Monica", "Phoebe", "Rachel", "Ross"
     );
+
+    private final Map<String, Phaser> personBarriers;
+    private final Map<String, List<Phrase>> personSpeech;
+
+
     private record Phrase(String text, String previousPerson) {}
-    private final Map<String, List<Phrase>> speech = Map.of(
-            "Chandler", new ArrayList<>(),
-            "Joey", new ArrayList<>(),
-            "Monica", new ArrayList<>(),
-            "Phoebe", new ArrayList<>(),
-            "Rachel", new ArrayList<>(),
-            "Ross", new ArrayList<>()
-    );
+    private String firstPerson;
+
+    public Scene() {
+        personBarriers = PERSON.stream().collect(Collectors.toMap(Function.identity(), ignored -> new Phaser(1)));
+        personSpeech = PERSON.stream().collect(Collectors.toMap(Function.identity(), ignored -> new ArrayList<>()));
+    }
 
     public void read(final BufferedReader reader) throws IOException {
         String previousPerson = null;
@@ -36,8 +45,13 @@ public class Scene {
             if (!PERSON.contains(person)) {
                 throw new IllegalArgumentException(STR."Invalid name: \{person}");
             }
-            speech.get(person).add(new Phrase(line.substring(index + 1), previousPerson));
+            personSpeech.get(person).add(new Phrase(line.substring(index + 1), previousPerson));
             previousPerson = person;
+        }
+    }
+
+    public void write(final BufferedWriter writer) throws IOException {
+        try (ExecutorService executor = Executors.newFixedThreadPool(PERSON.size())) {
         }
     }
 }
